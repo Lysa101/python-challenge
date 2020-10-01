51461cc0a27ec04f9f274785d1f21288ff7834dd
Lysa101 <70134436+Lysa101@users.noreply.github.com>
2020-10-01 14:34:54 -0600

    total months and profit

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,18 +1,25 @@
 import os
 import csv
-csvpath=os.path.join('Resources','budget_data.csv')
+csvpath = os.path.join('Resources', 'budget_data.csv')
 
-with open(csvpath, 'r') as file_handler:
-     lines = file_handler.read()
-     print(lines)
-     print(type(lines))
+with open(csvpath, newline='') as csvfile:
 
-mylist = ['Date','Profit/Losses']
-print(mylist)
+    # CSV reader specifies delimiter and variable that holds contents
+    csvreader = csv.reader(csvfile, delimiter=',')
+    csvheader=next(csvfile)
 
-#for row in open ('budget_data.csv'):
-    #num_rows += 1
-    #print(num_rows)
-#mylist=['Date','Profit/Losses']
-Months_count = mylist.count('Date')
-print('Total Months': Months_count) 
+    profit_loss=[]
+    count=[]
+    monthcount=0
+
+    for row in csvreader:
+        count.append(row[0])
+        monthcount=monthcount+1
+ 
+#net total of profit/loss
+        profit_loss.append(int(row[1]))
+        total_profit_loss=int(sum(profit_loss))
+
+    print(total_profit_loss)
+    print(monthcount)
+    
