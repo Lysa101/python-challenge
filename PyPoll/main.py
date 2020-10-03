ad3f9e2d377bc788c803bfe4ef5af18c9e1aec62
Lysa101 <70134436+Lysa101@users.noreply.github.com>
2020-10-03 10:06:25 -0600

    remove errors

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,35 +2,43 @@ import os
 import csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 
+votes=0
+vote_percentage = []
+winner=[]
+candidatedict = {"Khan" : 0,"Correy": 0,"Li": 0,"O_Tooley": 0}
+candidate=(candidatedict)
+#votes.append=candidatedict + 1
+total_votes=int(votes)
+    
+
 with open(csvpath, newline='') as csvfile:
 
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
     csvheader=next(csvfile)
+    print(csvheader)
+
 
     #assign of data 
-    votes.append=(candidate[0])
-    candidatedict = {"Khan","Correy","Li","O_Tooley"}
-    total_votes=(len(votes)
-    candidate=(candidatedict[2])
-    vote_percentage = []
-    winner=[]
+    votes +=1
+    
+    
    
     #total number of votes
     
     
-    for row in(csvreader):
-        votes =+1
+    #for row in(csvreader):
+        #votes =+1
 
-        if candidate in candidatedict:
-            {"Khan":votes,
-            "Li":votes,
-            "Correy":votes,
-            "O_Tooley": votes}
+        #if candidate in candidatedict:
+            #{"Khan":votes,
+            #"Li":votes,
+            #"Correy":votes,
+            #"O_Tooley": votes}
         
         
         
 
-        total_votes.append(int(row[1])
+        #total_votes.append(int(row[1]))
     
     #print(total_votes)
