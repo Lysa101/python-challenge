a20e45e81e2e852326c536e2222692fdf4995b1c
Lysa101 <70134436+Lysa101@users.noreply.github.com>
2020-10-05 00:24:32 -0600

    percentages and totals

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,11 +3,13 @@ import csv
 csvpath = os.path.join('Resources', 'election_data.csv')
 
 #assign data fields/variables/dictionary
-votes=0
+votes = 0
 vote_percentage = []
-winner=[]
-candidatedict = [{"Khan": 0,"Correy": 0,"Li": 0,"O_Tooley": 0}]
-#votes.append=candidatedict + 1
+winner = []
+Khan = 0
+Correy =  0 
+Li = 0 
+O_Tooley = 0
 total_votes=int(votes)
     
 
@@ -21,6 +23,7 @@ with open(csvpath, newline='') as csvfile:
 #tally votes for candidates
     for candidate in csvreader:
         total_votes=total_votes + 1
+
         if candidate [2] =="Khan":
             Khan = Khan + 1
 
@@ -31,18 +34,23 @@ with open(csvpath, newline='') as csvfile:
             Li = Li + 1
 
         elif candidate [2] == "O_Tooley":
-            O_Tooley = O_Tooley
+            O_Tooley = O_Tooley + 1
 
-               #votes +=1
+    #calculate % of votes per candidate
+
+    kpercent = round((Khan / total_votes),2)
+    cpercent = round((Correy / total_votes),2)
+    lpercent = round((Li / total_votes),2)
+    tpercent = round((O_Tooley / total_votes),2)
     
     print("Election Results")
     print("******************")
-    print("Total Votes:")
+    print("Total Votes:"+str(total_votes))
     print("*******************")
-    print("Khan:")
-    print("Correy:")
-    print("Li:")
-    print("O'Tooley:")
+    print(f"Khan : {kpercent:.2%} ({Khan})")
+    print(f"Correy :{cpercent: 2%} ({Correy})")
+    print(f"Li : {lpercent:.2%} ({Li})")
+    print(f"O_Tooley : {tpercent:.2%} ({O_Tooley})")
     print("********************")
     print("Winner:")
     print("********************")
